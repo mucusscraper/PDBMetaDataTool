20370df942d23105a7d819326813f371c6fa7e77
mucusscraper <daniellacerda201431@gmail.com>
2026-01-14 10:43:07 -0300

    B: updated to include info about non-polymers

--- internal/get_data/getdata.go
+++ internal/get_data/getdata.go
@@ -2,14 +2,16 @@ package getdata
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 type ProteinIssue struct {
-	ID          string            `json:"rcsb_id"`
-	AccessInfo  RcsbAccessionInfo `json:"rcsb_accession_info"`
-	ArticleInfo ArticleAccessInfo `json:"rcsb_primary_citation"`
-	ExptlInfo   ExptlAccessInfo   `json:"exptl"`
+	ID           string             `json:"rcsb_id"`
+	AccessInfo   RcsbAccessionInfo  `json:"rcsb_accession_info"`
+	ArticleInfo  ArticleAccessInfo  `json:"rcsb_primary_citation"`
+	ExptlInfo    []ExptlAccessInfo  `json:"exptl"`
+	EntitiesInfo EntitiesAccessInfo `json:"rcsb_entry_container_identifiers"`
 }
 
 type RcsbAccessionInfo struct {
@@ -25,17 +27,84 @@ type ExptlAccessInfo struct {
 	Method string `json:"method"`
 }
 
-func GetIssueData(url string) (ProteinIssue, error) {
+type EntitiesAccessInfo struct {
+	PolymerID    []string `json:"polymer_entity_ids"`
+	NonPolymerID []string `json:"non_polymer_entity_ids"`
+}
+
+type NonPolymerIssue struct {
+	Entity NameEntityNonPolymerAccession `json:"pdbx_entity_nonpoly"`
+	Data   DataEntityNonPolymerAccession `json:"rcsb_nonpolymer_entity"`
+}
+
+type NameEntityNonPolymerAccession struct {
+	Name   string `json:"name"`
+	CompID string `json:"comp_id"`
+}
+
+type DataEntityNonPolymerAccession struct {
+	FormulaWeight     float32 `json:"formula_weight"`
+	Description       string  `json:"pdbx_description"`
+	NumberOfMolecules int     `json:"pdbx_number_of_molecules"`
+}
+
+func GetIssueDataEntry(url string) (ProteinIssue, error, []string, []string) {
 	res, err := http.Get(url)
 	if err != nil {
-		return ProteinIssue{}, err
+		fmt.Printf("Error here 1\n")
+		return ProteinIssue{}, err, nil, nil
 	}
 	defer res.Body.Close()
-	var protein ProteinIssue
+	var PDB ProteinIssue
 	decoder := json.NewDecoder(res.Body)
-	err = decoder.Decode(&protein)
+	err = decoder.Decode(&PDB)
 	if err != nil {
-		return ProteinIssue{}, err
+		fmt.Printf("Error here 2\n")
+		return ProteinIssue{}, err, nil, nil
 	}
-	return protein, nil
+	var list_of_urls_polymers []string
+	for _, polymer_id := range PDB.EntitiesInfo.PolymerID {
+		list_of_urls_polymers = append(list_of_urls_polymers, fmt.Sprintf("https://data.rcsb.org/rest/v1/core/polymer_entity/7o52/%v", polymer_id))
+	}
+	var list_of_urls_non_polymers []string
+	for _, non_polymer_id := range PDB.EntitiesInfo.NonPolymerID {
+		list_of_urls_non_polymers = append(list_of_urls_non_polymers, fmt.Sprintf("https://data.rcsb.org/rest/v1/core/nonpolymer_entity/7o52/%v", non_polymer_id))
+	}
+	return PDB, nil, list_of_urls_polymers, list_of_urls_non_polymers
+}
+
+func GetDataForNonPolymers(url string) (NonPolymerIssue, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return NonPolymerIssue{}, err
+	}
+	defer res.Body.Close()
+	var NonPolymer NonPolymerIssue
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&NonPolymer)
+	if err != nil {
+		return NonPolymerIssue{}, err
+	}
+	return NonPolymer, nil
+}
+
+func GetEntitiesURL(url string) ([]string, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error here 3\n")
+		return nil, err
+	}
+	defer res.Body.Close()
+	var PDB ProteinIssue
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&PDB)
+	if err != nil {
+		fmt.Printf("Error here 3\n")
+		return nil, err
+	}
+	var list_of_urls []string
+	for _, entity := range PDB.EntitiesInfo.PolymerID {
+		list_of_urls = append(list_of_urls, fmt.Sprintf("curl https://data.rcsb.org/rest/v1/core/polymer_entity/7urx/%v", entity))
+	}
+	return list_of_urls, nil
 }
--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 	getdata "github.com/mucusscraper/pdb_metadata_tool/internal/get_data"
 )
 
-const issueURL = "https://data.rcsb.org/rest/v1/core/entry/7urx"
+const issueURL = "https://data.rcsb.org/rest/v1/core/entry/7o52"
 
 func main() {
 	fmt.Printf("Hello World test!\n")
-	res, err := getdata.GetIssueData(issueURL)
+	res, err, list_of_urls_polymer, list_of_urls_non_polymers := getdata.GetIssueDataEntry(issueURL)
 	if err != nil {
 		fmt.Printf("Error getting data\n")
 		return
@@ -19,4 +19,29 @@ func main() {
 	fmt.Printf("%v\n", res.AccessInfo.DepositDate)
 	fmt.Printf("%v\n", res.ArticleInfo.DOI)
 	fmt.Printf("%v\n", res.ArticleInfo.Title)
+	for _, method := range res.ExptlInfo {
+		fmt.Printf("%v\n", method)
+	}
+	for _, polymer_id := range res.EntitiesInfo.PolymerID {
+		fmt.Printf("%v\n", polymer_id)
+	}
+	for _, non_polymer_id := range res.EntitiesInfo.NonPolymerID {
+		fmt.Printf("%v\n", non_polymer_id)
+	}
+	for _, polymer_url := range list_of_urls_polymer {
+		fmt.Printf("%v\n", polymer_url)
+	}
+	for _, non_polymer_url := range list_of_urls_non_polymers {
+		fmt.Printf("%v\n", non_polymer_url)
+		NonPolymer, err := getdata.GetDataForNonPolymers(non_polymer_url)
+		if err != nil {
+			fmt.Printf("Error getting data")
+			return
+		}
+		fmt.Printf("NAME: %v\n", NonPolymer.Entity.Name)
+		fmt.Printf("COMP ID: %v\n", NonPolymer.Entity.CompID)
+		fmt.Printf("DESCRIPTION: %v\n", NonPolymer.Data.Description)
+		fmt.Printf("FORMULA WEIGHT: %v\n", NonPolymer.Data.FormulaWeight)
+		fmt.Printf("NUMBER OF MOLECULES: %v\n", NonPolymer.Data.NumberOfMolecules)
+	}
 }
